f4eabb535dfd67b5eed9f5319257581190e42130
KXCG Maas <boy.maas@gmail.com>
2010-06-04 03:38:35 +0200

    Basic setup for a minimal web dsl enabling rapid developement of reusable web blocks.

---
+++ project.clj
@@ -0,0 +1,10 @@
+(defproject clocks "1.0.0-SNAPSHOT"
+  :description "FIXME: write"
+  :dependencies [[org.clojure/clojure "1.1.0"]
+                 [compojure "0.4.0-RC3"] 
+                 [ring/ring-devel "0.2.0"] 
+                 [ring/ring-jetty-adapter "0.2.0"] 
+                 [hiccup "0.2.4"] 
+                 [org.clojure/clojure-contrib "1.1.0"]]
+  :dev-dependencies  [[swank-clojure "1.2.0"]] 
+  )
