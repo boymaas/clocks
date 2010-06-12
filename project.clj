2e50c310846fb9c9597abeb0393bb600b8eb844d
Boy Maas <boy.maas@gmail.com>
2010-06-12 04:25:01 +0200

    lein-clojars

--- project.clj
+++ project.clj
@@ -1,17 +1,15 @@
-(defproject clocks "1.0.0-SNAPSHOT"
-  :description "FIXME: write"
+(defproject clocks "0.5"
+  :description "A webdsl on top of compojure, see: http://github.com/boymaas/clocks/"
   :dependencies [
                  [org.clojure/clojure "1.2.0-master-SNAPSHOT"]
                  [org.clojure/clojure-contrib "1.2.0-SNAPSHOT"]
-                 ;[org.clojure/clojure "1.1.0"]
-                 ;[org.clojure/clojure-contrib "1.1.0"]
                  [compojure "0.4.0-RC3"] 
                  [ring/ring-devel "0.2.0"] 
                  [ring/ring-jetty-adapter "0.2.0"] 
                  [hiccup "0.2.4"] 
                  [scriptjure "0.1.8"]
-                 ;[org.clojars.ato/scriptjure "0.1.0-SNAPSHOT"]
                  ]
-                 
-  :dev-dependencies  [[swank-clojure "1.2.0"]] 
+  :dev-dependencies  [[swank-clojure "1.2.0"] 
+                      [lein-clojars "0.5.0"]]
+  :repositories [["clojars" "http://clojars.org/repo"]]
   )
